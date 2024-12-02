a4f2ce9ca3b849712aee80b00ae4938adf9cb592
dominiclake <144931780+dominiclake@users.noreply.github.com>
2024-12-02 18:20:44 -0500

    fixed CORS issue

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import torch
 import joblib
 from model import SMSClassifier
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # -------------------------------
 # Load Deep Learning Model
